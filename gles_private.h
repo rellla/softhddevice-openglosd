4713e6e9595350e0c82789ad395753c3e5370d68
Andreas Baierl <ichgeh@imkreisrum.de>
2021-01-15 17:00:27 +0100

    fixup! Move GL init to video_drm.c

--- gles_private.h
+++ gles_private.h
@@ -40,7 +40,7 @@ struct gbm {
 
 void eglCheckError(const char *stmt, const char *fname, int line);
 void glCheckError(const char *stmt, const char *fname, int line);
-void eglAquireContext(void);
+void eglAcquireContext(void);
 void eglReleaseContext(void);
 #ifdef WRITE_PNG
 int writeImage(char* filename, int width, int height, void *buffer, char* title);
